f69beb3f35cadda247a58da9da35c76b637bf19e
RomiTT <chikyom.kim@gmail.com>
2018-11-24 14:22:11 +0900

    Applied logging with Serilog.

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 
-namespace aspnetcore_graphql_auth {
-    public class Startup {
+namespace aspnetcore_graphql_auth
+{
+  public class Startup {
+        public Startup(IConfiguration configuration, IHostingEnvironment env) {
+            Configuration = configuration;
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.RollingFile($"{env.ContentRootPath}/log/" + "log-{Date}.txt",
+                    fileSizeLimitBytes : 1_000_000,
+                    shared : true,
+                    flushToDiskInterval : TimeSpan.FromSeconds(1))
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) { }
