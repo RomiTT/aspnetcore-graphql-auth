8fd19747be6f8cb4c796bec1bac1d05ae5e1333b
RomiTT <chikyom.kim@gmail.com>
2018-11-27 15:21:48 +0900

    Set default value to the Token property of User class.

--- Models/User.cs
+++ Models/User.cs
@@ -12,6 +12,6 @@ namespace Bowgum.Models {
         public UserRole Role { get; set; }
 
         [NotMapped]
-        public string Token { get; set; }
+        public string Token { get; set; } = "";
     }
 }
